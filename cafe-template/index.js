5fc202812487e1e618b2061fe04b1ea79753e1b5
Nisha Tiwari <nishatiwari1719@gmail.com>
2022-05-24 21:25:17 +0530

    all files committed

---
+++ cafe-template/index.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("eat");
+var y = document.getElementById("drink");
+var z = document.getElementById("btn");
+
+    function showDrinkMenu()
+    {
+        x.style.left = "-520px";
+        y.style.left = "50px";
+        z.style.left = "110px";
+    }
+
+    function showEatMenu()
+    {
+        x.style.left = "50px";
+        y.style.left = "650px";
+        z.style.left = "0px";
+    }
